786e3a1e7633bc4be368585cd6889666acb71111
Patiphan Thiansuwan <newnismokung_@hotmail.com>
2022-02-09 12:57:03 +0700

    [UPDATE+FIX]8 readme gitignore

--- main/provider.tf
+++ main/provider.tf
@@ -1,19 +1,27 @@
-provider "aws" { # Cloud provider
-  profile = "produser"
-  region  = "ap-southeast-1"
-}
-
 terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 3.0"
     }
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = ">= 1.7.0"
+    }
   }
+}
+
+provider "aws" {
+  profile = "produser"
+  region  = "ap-southeast-1"
 }
 
 provider "helm" {
   kubernetes {
     config_path = "~/.kube/config"
   }
+}
+
+provider "kubernetes" {
+  config_path = "~/.kube/config"
 }
